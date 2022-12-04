46870ab78f48dbd3a0f525093d9e45f927a64055
IliaSyrytskii <polonamocyi2525@gmail.com>
2022-12-04 19:58:51 +0300

    bad commit

--- src/Ex6.java
+++ src/Ex6.java
@@ -1,19 +1,24 @@
 import java.util.Arrays;
-
 public class Ex6 {
     public static void main (String[] args){
 
         int[][] array = new int[][]{{10, 4, 2, 5},{2, 6, 2, 1}};
 
-        for (int t = 0; t < array.length; t++){
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array.length; j++) {
-                    if (array[t][i] < array[t][j]) {
-                        int result = array[t][i];
-                        array[t][i] = array[t][j];
-                        result = array[t][j];
-                    }
-                }
+
+        int[] temp=new int[8];
+        int k=0;
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                temp[k]=array[i][j];
+                k++;
+            }
+        }
+        Arrays.sort(temp);
+        k=-1;
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                k++;
+                array[i][j]=temp[k];
             }
         }
         System.out.println(Arrays.deepToString(array));
