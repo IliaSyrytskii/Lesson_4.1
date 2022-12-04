93cae3f2d6d8ab6f8a9841e3fa34fbb4754ef331
IliaSyrytskii <polonamocyi2525@gmail.com>
2022-12-04 20:04:16 +0300

    end

--- src/Ex6.java
+++ src/Ex6.java
@@ -5,20 +5,20 @@ public class Ex6 {
         int[][] array = new int[][]{{10, 4, 2, 5},{2, 6, 2, 1}};
 
 
-        int[] temp=new int[8];
-        int k=0;
-        for(int i=0;i<array.length;i++){
-            for(int j=0;j<array[i].length;j++){
-                temp[k]=array[i][j];
+        int[] temp = new int[8];
+        int k = 0;
+        for(int i = 0; i < array.length;i++){
+            for(int j=0; j < array[i].length; j++){
+                temp[k] = array[i][j];
                 k++;
             }
         }
         Arrays.sort(temp);
-        k=-1;
-        for(int i=0;i<array.length;i++){
-            for(int j=0;j<array[i].length;j++){
+        k = -1;
+        for(int i = 0; i < array.length; i++){
+            for(int j=0; j < array[i].length; j++){
                 k++;
-                array[i][j]=temp[k];
+                array[i][j] = temp[k];
             }
         }
         System.out.println(Arrays.deepToString(array));
