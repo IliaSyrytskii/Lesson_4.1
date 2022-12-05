1f350c6393eabfa4a324473d7117ff3492d32bb2
IliaSyrytskii <polonamocyi2525@gmail.com>
2022-12-05 15:19:30 +0300

    reload ex5

--- src/Ex5.java
+++ src/Ex5.java
@@ -1,14 +1,22 @@
+import java.util.Arrays;
+
 public class Ex5 {
     public static void main (String[] args){
-        int[][] array = new int[][]{{1, 3, 5}, {2, 4, 6}, {7, 5, 3}};
+        int[][][] array = new int[][][]{{{1, 3, 5}, {2, 4, 6}}, {{7, 5, 3}, {6, 4, 2}}};
 
-        for (int t = 0; t < array.length; t++){
+        System.out.println(Arrays.deepToString(array));
+
+        for (int t = 0; t < array.length; t++) {
             for (int i = 0; i < array[t].length; i++) {
-                if (t == i) {
-                    System.out.println(t + array[t][i]);
+                for (int j = 0; j < array[i].length; j++) {
+                    if ((t == i) & (i == j)) {
+                        System.out.println(t + i + array[t][i][j]);
+                    }
+                    if ((t != i) & (i != j)) {
+                        System.out.println(j + t + array[t][i][j]);
+                    }
                 }
             }
-            System.out.println();
         }
     }
 }
